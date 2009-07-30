91043533edc66200b7d86af95a88761c901ed1e3
David Sauve <dnsauve@gmail.com>
2009-07-30 09:01:38 -0400

    Corrected year in copyright notice.  Should have been 2009, not 2007

--- tests/settings.py
+++ tests/settings.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2007 David Sauve
+# Copyright (C) 2009 David Sauve
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
