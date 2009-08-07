c496682895869a02faf20ba6f7faea6acbf42653
David Sauve <dnsauve@gmail.com>
2009-08-07 08:12:15 -0400

    Updated tests to work with latest changes in Haystack.

--- tests/search_sites.py
+++ tests/search_sites.py
@@ -14,20 +14,5 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import os
-from django.conf import settings
-
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = 'xapian_tests.db'
-
-INSTALLED_APPS = (
-    'haystack',
-    'xapian_haystack.tests',
-)
-
-ROOT_URLCONF = 'tests.urls'
-
-HAYSTACK_SITECONF = 'tests.search_sites'
-HAYSTACK_SEARCH_ENGINE = 'xapian'
-HAYSTACK_XAPIAN_PATH = os.path.join('tmp', 'test_xapian_query')
-HAYSTACK_INCLUDE_SPELLING = True
+import haystack
+haystack.autodiscover()
