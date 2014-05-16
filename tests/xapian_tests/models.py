8c805b72e55e337146c906aa4e0309f94fae0458
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-05-17 00:09:23 +0200

    Added tests to address issue #123.

--- tests/xapian_tests/models.py
+++ tests/xapian_tests/models.py
@@ -1,2 +1,11 @@
-# Copyright (C) 2009, 2010, 2011, 2012 David Sauve
-# Copyright (C) 2009, 2010 Trapeze
+from django.db import models
+
+
+class Document(models.Model):
+    type_name = models.CharField(max_length=50)
+    number = models.IntegerField()
+
+    date = models.DateField()
+
+    summary = models.TextField()
+    text = models.TextField()
---
+++ tests/xapian_tests/search_indexes.py
@@ -0,0 +1,22 @@
+from haystack import indexes
+
+import models
+
+
+class DocumentIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True)
+    summary = indexes.CharField(model_attr='summary')
+
+    type_name = indexes.CharField(model_attr='type_name')
+
+    number = indexes.IntegerField(model_attr='number')
+
+    name = indexes.CharField()
+    date = indexes.DateField(model_attr='date')
+
+    def get_model(self):
+        return models.Document()
+
+    def prepare_name(self, obj):
+        return "%s %s" % (obj.type_name, str(obj.number))
+
