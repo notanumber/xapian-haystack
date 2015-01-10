845c71e549155928e0b3a2dcb03aff2635cdb425
Jürg Rast <juergr@gmail.com>
2015-01-10 19:57:30 +0100

    Updated tests with a test wich shows more clearly the suspected bug

--- tests/xapian_tests/tests/test_stemming.py
+++ tests/xapian_tests/tests/test_stemming.py
@@ -23,14 +23,14 @@ class StemmingTestCase(TestCase):
     def setUp(self):
         super(StemmingTestCase, self).setUp()
         # Words (especialy nouns) start with an upper case letter in some languages (German, ...)
-        words = ['Connection', 'connection']
+        words = ['Connection', 'connection', 'Plattenkreuz']
 
-        for i in range(1, 3):
+        for i in range(len(words)):
             doc = Document()
-            doc.number = i
+            doc.number = i + 1
             doc.date = datetime.date.today()
-            doc.name = words[i - 1]
-            doc.text = words[i - 1]
+            doc.name = words[i]
+            doc.text = words[i]
             doc.save()
 
         self.index = DocumentIndex()
@@ -47,7 +47,7 @@ class StemmingTestCase(TestCase):
         super(StemmingTestCase, self).tearDown()
 
     def test_stemming(self):
-        object_ids = set(pks(Document.objects.all()))
+        object_ids = set(pks(Document.objects.filter(number__lte=2)))
         # Searches for the exact term sould return both documents
         self.assertEqual(set(pks(self.queryset.auto_query("Connection"))), object_ids)
         self.assertEqual(set(pks(self.queryset.auto_query("connection"))), object_ids)
@@ -55,37 +55,11 @@ class StemmingTestCase(TestCase):
         self.assertEqual(set(pks(self.queryset.auto_query("connect"))), object_ids)
         self.assertEqual(set(pks(self.queryset.auto_query("Connect"))), object_ids)
 
-
-class StemmingEdgeNgramTestCase(TestCase):
-
-    def setUp(self):
-        super(StemmingEdgeNgramTestCase, self).setUp()
-
-        doc = Document()
-        doc.number = 1
-        doc.date = datetime.date.today()
-        doc.name = 'Plattenkreuz'
-        doc.text = ''
-        doc.save()
-
-        self.index = DocumentIndex()
-        self.ui = connections['default'].get_unified_index()
-        self.ui.build(indexes=[self.index])
-
-        self.backend = connections['default'].get_backend()
-        self.backend.update(self.index, Document.objects.all())
-
-        self.queryset = SearchQuerySet()
-
-    def tearDown(self):
-        Document.objects.all().delete()
-        super(StemmingEdgeNgramTestCase, self).tearDown()
-
-    def test_stemmeing(self):
-        object_ids = set(pks(Document.objects.all()))
-        # Searches for the exact term sould return both documents
-        self.assertEqual(set(pks(self.queryset.auto_query("Plattenkreuz"))), object_ids)
-        self.assertEqual(set(pks(self.queryset.auto_query("plattenkreuz"))), object_ids)
-        # Same should apply for the stemmed values
-        self.assertEqual(set(pks(self.queryset.auto_query("Plat"))), object_ids)
-        self.assertEqual(set(pks(self.queryset.auto_query("plat"))), object_ids)
+    def test_stemming_edgengram_field(self):
+        obj = set(pks(Document.objects.filter(name='Plattenkreuz')))
+        # Searches for the exact term sould find the correct object
+        self.assertEqual(set(pks(self.queryset.auto_query("Plattenkreuz"))), obj)
+        self.assertEqual(set(pks(self.queryset.auto_query("plattenkreuz"))), obj)
+        # Same should apply for partial matches
+        self.assertEqual(set(pks(self.queryset.auto_query("Plat"))), obj)
+        self.assertEqual(set(pks(self.queryset.auto_query("plat"))), obj)
