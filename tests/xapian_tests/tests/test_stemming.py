4873f7f073b1c6d13110fae77cdb6430e4eee9d9
Jürg Rast <juergr@gmail.com>
2015-01-10 17:18:51 +0100

    Created simple Test for stemming

---
+++ tests/xapian_tests/tests/test_stemming.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import datetime
+from django.test import TestCase
+
+from haystack.query import SearchQuerySet
+from haystack import indexes, connections
+
+from xapian_tests.models import Document
+from xapian_tests.tests.test_backend import pks
+
+
+class DocumentIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True)
+    word = indexes.CharField(model_attr='name')
+
+    def get_model(self):
+        return Document
+
+
+class StemmingTestCase(TestCase):
+
+    def setUp(self):
+        super(StemmingTestCase, self).setUp()
+        words = ['Connection', 'connection']
+
+        for i in range(1, 3):
+            doc = Document()
+            doc.number = i
+            doc.date = datetime.date.today()
+            doc.name = words[i - 1]
+            doc.text = words[i - 1]
+            doc.save()
+
+        self.index = DocumentIndex()
+        self.ui = connections['default'].get_unified_index()
+        self.ui.build(indexes=[self.index])
+
+        self.backend = connections['default'].get_backend()
+        self.backend.update(self.index, Document.objects.all())
+
+        self.queryset = SearchQuerySet()
+
+    def tearDown(self):
+        Document.objects.all().delete()
+        super(StemmingTestCase, self).tearDown()
+
+    def test_stemmeing(self):
+        object_ids = set(pks(Document.objects.all()))
+        self.assertEqual(set(pks(self.queryset.auto_query("Connection"))), object_ids)
+        self.assertEqual(set(pks(self.queryset.auto_query("connection"))), object_ids)
+        self.assertEqual(set(pks(self.queryset.auto_query("connect"))), object_ids)
+        self.assertEqual(set(pks(self.queryset.auto_query("Connect"))), object_ids)
